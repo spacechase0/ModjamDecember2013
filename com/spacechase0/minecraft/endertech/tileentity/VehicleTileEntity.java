6facdcbbc1a3507dab7f605890b21efbe85c11b6
Chase W <chasew@silkweaver.com>
2013-12-15 18:24:47 -0500

    Saving and loading, untested.

--- com/spacechase0/minecraft/endertech/tileentity/VehicleTileEntity.java
+++ com/spacechase0/minecraft/endertech/tileentity/VehicleTileEntity.java
@@ -3,6 +3,8 @@ package com.spacechase0.minecraft.endertech.tileentity;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagIntArray;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.network.INetworkManager;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.Packet132TileEntityData;
@@ -48,13 +50,13 @@ public class VehicleTileEntity extends TileEntity
 					short data = getBlockData( id, meta );
 					
 					TileEntity te = worldObj.getBlockTileEntity( ix, iy, iz );
-					worldObj.removeBlockTileEntity( ix, iy, iz );
+					te.xCoord = ix;
+					te.yCoord = iy;
+					te.zCoord = iz;
 					
 					int index = ix + ( iy * size ) + ( iz * size * size );
 					blockData[ index ] = data;
 					blockTiles[ index ] = te;
-					worldObj.removeBlockTileEntity( ix, iy, iz );
-					worldObj.setBlock( ix, iy, iz, 0, 0, 0x2 );
 				}
 			}
 		}
@@ -76,12 +78,69 @@ public class VehicleTileEntity extends TileEntity
     public void readFromNBT( NBTTagCompound tag )
     {
     	super.readFromNBT( tag );
+    	
+    	if ( tag.hasKey( "BlockData" ) )
+    	{
+    		int[] data = tag.getIntArray( "BlockData" );
+    		
+    		int size = ( int ) Math.cbrt( data.length );
+    		
+    		blockData = new short[ data.length ];
+    		blockTiles = new TileEntity[ data.length ];
+    		
+    		for ( int i = 0; i < data.length; ++i )
+    		{
+    			int num = data[ i ];
+    			blockData[ ( i * 2 ) + 0 ] = decompressA( num );
+    			blockData[ ( i * 2 ) + 1 ] = decompressB( num );
+    		}
+    		
+    		if ( tag.hasKey( "BlockTiles" ) )
+    		{
+    			NBTTagList list = ( NBTTagList ) tag.getTag( "BlockTiles" );
+    			for ( int i = 0; i < list.tagCount(); ++i )
+    			{
+    				NBTTagCompound nbt = ( NBTTagCompound ) list.tagAt( i );
+    				TileEntity te = TileEntity.createAndLoadEntity( nbt );
+
+					int index = te.xCoord + ( te.yCoord * size ) + ( te.zCoord * size * size );
+					blockTiles[ index ] = te;
+    			}
+    		}
+    	}
     }
 
 	@Override
     public void writeToNBT( NBTTagCompound tag )
     {
 		super.writeToNBT( tag );
+		
+		if ( blockData.length > 0 )
+		{
+			int[] data = new int[ blockData.length / 2 ];
+			for ( int i = 0; i < blockData.length / 2; ++i )
+			{
+				int indexBase = i * 2;
+				data[ i ] = compress( blockData[ indexBase + 0 ], blockData[ indexBase + 1 ] );
+			}
+			
+			tag.setIntArray( "BlockData", data );
+		}
+		
+		NBTTagList tileEntities = new NBTTagList();
+		for ( TileEntity te : blockTiles )
+		{
+			if ( te == null ) continue;
+			
+			NBTTagCompound nbt = new NBTTagCompound();
+			te.writeToNBT( nbt );
+			
+			tileEntities.appendTag( nbt );
+		}
+		if ( tileEntities.tagCount() > 0 )
+		{
+			tag.setTag( "BlockTiles", tileEntities );
+		}
     }
 	
 	private int getBlockId( short data )
@@ -97,6 +156,21 @@ public class VehicleTileEntity extends TileEntity
 	private short getBlockData( int id, int meta )
 	{
 		return ( short )( ( id & 0x0FFF ) | ( ( meta << 12 ) & 0xF000 ) );
+	}
+	
+	private int compress( short a, short b )
+	{
+		return ( short )( a | ( b << 16 ) );
+	}
+	
+	private short decompressA( int num )
+	{
+		return ( short )( num & 0x0000FFFF );
+	}
+	
+	private short decompressB( int num )
+	{
+		return ( short )( ( num >> 16 ) & 0x0000FFFF );
 	}
 	
 	private short[] blockData = new short[ 0 ];
